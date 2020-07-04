2e9105b3a3c10368400d0e07bc7b5f30c18860b8
flannois <kaox@debian-pc.home>
2020-07-04 17:03:04 +0200

    Ajout gitignore

--- .buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/dsp/lossless_enc_msa.o.d
+++
@@ -1,8 +0,0 @@
-/home/kaox/calculmental/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/dsp/lossless_enc_msa.o: \
-  /home/kaox/calculmental/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/dsp/lossless_enc_msa.c \
-  /home/kaox/calculmental/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/dsp/dsp.h \
-  /home/kaox/calculmental/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/types.h
-
-/home/kaox/calculmental/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/dsp/dsp.h:
-
-/home/kaox/calculmental/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/types.h:
--- .buildozer/android/platform/build-armeabi-v7a/build/python-installs/CalculMental/kivy/version.py
+++
@@ -1,4 +0,0 @@
-# THIS FILE IS GENERATED FROM KIVY SETUP.PY
-__version__ = '1.11.1'
-__hash__ = 'fbf1083de40423419e316aa268ba020f6b1d1cfa'
-__date__ = '20200703'
