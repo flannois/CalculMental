dcd747baaeea0a68280f26108e4f80cdd8482cb5
flannois <kaox@debian-pc.home>
2020-07-06 18:31:56 +0200

    Test avec images pour boutons

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ class Fenetre(BoxLayout):
             self.labelnb1.text = str("")
             self.labeloperant.text = str("Perdu")
             self.labelnb2.text = str("")
-            self.labelnombre.text = str("Ton score\n{}".format(self.score))
+            self.labelnombre.text = str("Score : {}".format(self.score))
         
 
 
