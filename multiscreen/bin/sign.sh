5d1e88e761def27734ecdbcd4e8dff5e90932ae8
flannois <florent.lannois@gmail.com>
2020-12-17 20:05:38 +0100

    A comparer appli fonctionnelle et multiscreen

---
+++ multiscreen/bin/sign.sh
@@ -0,0 +1,3 @@
+apksigner sign --ks calculmental-key.keystore --in CalculMental-0.7-arm64-v8a-release-unsigned.apk --out calculMental-sign-arm64.apk
+apksigner sign --ks calculmental-key.keystore --in CalculMental-0.7-armeabi-v7a-release-unsigned.apk --out calculMental-sign-armeabi.apk
+
